1b382c810c062a24d9bd94f167667833297a1167
SolidityX <88942434+solidityx@users.noreply.github.com>
2023-12-12 20:54:25 +0530

    Update Params.sol
    commented WithdrawProfitPeriod

--- Params.sol
+++ Params.sol
@@ -17,7 +17,7 @@ contract Params {
     // Validator have to wait StakingLockPeriod blocks to withdraw staking
     uint64 public constant StakingLockPeriod = 86400;
     // Validator have to wait WithdrawProfitPeriod blocks to withdraw his profits
-    uint64 public constant WithdrawProfitPeriod = 28800;
+   // uint64 public constant WithdrawProfitPeriod = 28800;
     uint256 public constant MinimalStakingCoin = 32 ether;
     // minimum initial staking to become a validator
     uint256 public constant minimumValidatorStaking = 1000000 ether;
